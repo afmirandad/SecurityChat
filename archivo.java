bce6ac92ccae3e1f5ffa11973b7c12f21815f9d1
afmirandadiaz <amirandadiaz@godaddy.com>
2025-08-11 22:13:53 -0500

    Vamos a ser serios

--- archivo.java
+++
@@ -1,9 +0,0 @@
-//Gracias Desarrollador, te quiero mucho!!!
-public void main(){
-    system.out.println("Hola Mundo");
-}
-
-//Funciona para mi
-public void saludo(){
-    return "Hi, how are you?";
-}
