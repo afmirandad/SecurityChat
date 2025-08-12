3e4fc398e0c76740ea5e4ee84f1e7fb5105adc7a
406 - Comunicaciones <406@laboratorios.usbbog.edu.co>
2025-08-11 19:02:36 -0500

    Features: Estimado, esto no funciona, arreglelo porfa!!!

--- archivo.java
+++ archivo.java
@@ -1,8 +1,9 @@
+//Mmmmmm como que esta tampoco
 public void main(){
     system.out.println("Hello world");
 }
 
-
+//Mmmm como que no funciona esta!!!
 public void saludo(){
     return "Hola, como estas?";
 }
